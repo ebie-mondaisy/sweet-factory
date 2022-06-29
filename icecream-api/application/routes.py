5651957c101bbb8e5afdad7e921f2d088565de71
ebs-mondaisy <radio@cyberdocker.europe-west2-c.c.symbolic-button-349713.internal>
2022-06-29 14:37:07 +0000

    EM: working on front end application

--- icecream-api/application/routes.py
+++ icecream-api/application/routes.py
@@ -8,29 +8,29 @@ def icecream():
     sweet_data = request.get_json()
     
     if sweet_data['cakes'] == 'Red Velvet':
-        count + 6
+        count += 6
     elif sweet_data['cakes'] == 'Victoria Sponge':
-        count + 5
+        count += 5
     elif sweet_data['cakes'] == 'Chocolate':
-        count + 4
+        count += 4
     elif sweet_data['cakes'] == 'Confetti':
-        count + 3
+        count += 3
     elif sweet_data['cakes'] == 'Bubblegum':
-        count + 2
+        count += 2
     elif sweet_data['cakes'] == 'Lemon Drizzle':
-        count + 1
+        count += 1
     elif sweet_data['shakes'] == 'Chocolate':
-        count + 1
+        count += 1
     elif sweet_data['shakes'] == 'Biscoff':
-        count + 2
+        count += 2
     elif sweet_data['shakes'] == 'Strawberry':
-        count + 3
+        count += 3
     elif sweet_data['shakes'] == 'Jammy Dodger':
-        count + 4
+        count += 4
     elif sweet_data['shakes'] == 'Vanilla':
-        count + 5
+        count += 5
     elif sweet_data['shakes'] == "Cookies 'n' Cream":
-        count + 6
+        count += 6
 
         return count
 
--- sweet-factory/application/routes.py
+++ sweet-factory/application/routes.py
@@ -4,8 +4,14 @@ from application.models import *
 import requests
 
 
-@app.route('/')
+@app.route('/', methods = ['GET'])
 def home():
-    cakes = requests.get('http://cake-api:5000/get_cake').text
-    shakes = requests.get('http://milkshake-api:5000/get_milkshake').text
-    cream = requests.post('http://icecream-api:5000/get_icecream')
+    cakes = requests.get('http://cake-api:5000/get_cake')
+    shakes = requests.get('http://milkshake-api:5000/get_milkshake')
+    cream = requests.post('http://icecream-api:5000/get_icecream', json = dict(cakes=cakes, shakes=shakes))
+    return render_template('home.html', cakes=cakes, shakes=shakes, cream=cream.text, title='Home')
+
+
+@app.route('/idea-library', methods=['GET'])
+def ideas():
+    pass
