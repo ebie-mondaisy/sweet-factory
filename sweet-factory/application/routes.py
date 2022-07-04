f71cb6847ecd6a24b08ab6a656db6e402fc74a22
ebie-mondaisy <radio@cyberdocker.europe-west2-c.c.symbolic-button-349713.internal>
2022-07-04 16:22:13 +0000

    EM: docker-compose is fixed!

--- sweet-factory/application/routes.py
+++ sweet-factory/application/routes.py
@@ -7,6 +7,6 @@ import requests, json
 def home():
     cakes = requests.get('http://cake-api:5000/get_cake').text
     shakes = requests.get('http://milkshake-api:5000/get_milkshake').text
-    tooth = requests.post('http://icecream-api:5000/get_toothache', json = dict(cakes=cakes, shakes=shakes))
+    tooth = requests.post('http://toothache-api:5000/get_toothache', json = dict(cakes=cakes, shakes=shakes))
 
     return render_template('home.html', cakes=cakes, shakes=shakes, tooth=tooth.text)
