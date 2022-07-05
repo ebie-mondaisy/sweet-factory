5f0dd19d1b74acb0d54ba15e92922c3c1244e946
ebie-mondaisy <radio@cyberdocker.europe-west2-c.c.symbolic-button-349713.internal>
2022-07-05 11:29:19 +0000

    EM: edited test.sh again

--- test.sh
+++ test.sh
@@ -1,8 +1,14 @@
 #!/bin/bash
-python3 -m venv venv
-
-source venv/bin/activate
-
-pip3 install -r requirements.txt
-
-python3 -m pytest --cov=. --cov-report xml -v
+declare -a directories=("sweet-factory" "cake-api" "milkshake-api" "toothache-api")
+for dir in "${directories[@]}"
+do
+  cd ${dir}
+  sudo apt-get update
+  sudo apt-get install python3 python3-pip python3.8-venv
+  python3 -m venv venv
+  source venv/bin/activate
+  pip3 install -r requirements.txt
+  python3 -m pytest --cov --cov-report xml -v
+  deactivate
+  cd ..
+done
