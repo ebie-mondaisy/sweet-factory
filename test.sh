63951a461da359817cf687357f9327b7c6df8924
ebie-mondaisy <radio@cyberdocker.europe-west2-c.c.symbolic-button-349713.internal>
2022-07-05 11:30:22 +0000

    EM: edited test.sh again

--- test.sh
+++ test.sh
@@ -3,8 +3,8 @@ declare -a directories=("sweet-factory" "cake-api" "milkshake-api" "toothache-ap
 for dir in "${directories[@]}"
 do
   cd ${dir}
-  sudo apt-get update
-  sudo apt-get install python3 python3-pip python3.8-venv
+  sudo apt update
+  sudo apt install python3 python3-pip python3.8-venv
   python3 -m venv venv
   source venv/bin/activate
   pip3 install -r requirements.txt
